9692df9415d2012c92e4ee715905efa77d55bbb0
kkammoo <rlaansus89@gmail.com>
2022-07-05 13:52:31 +0900

    220705

--- content/views.py
+++ content/views.py
@@ -9,7 +9,9 @@ class Main(APIView):
     def get(self, request):
         print("GET request")
 
-        feed_list = Feed.objects.all()  # select * from content_feed
+        # select * from content_feed (둥록된 순으로 출력 - 최근이 가장 아래)
+        # order by로 오름차순, 내림차순으로 정렬 가능 (디폴트는 오름차순, - 붙이면 내림차순)
+        feed_list = Feed.objects.all().order_by('-id')
         print(feed_list)
         for feed in feed_list:
             print(feed.content)
