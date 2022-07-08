489fca6d2360eb69aff1a3aadb4f259173c0bece
kkammoo <rlaansus89@gmail.com>
2022-07-08 18:05:27 +0900

    220708

--- content/views.py
+++ content/views.py
@@ -1,7 +1,13 @@
+import os
+from uuid import uuid4
+
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from instagramClone.settings import MEDIA_ROOT
 from .models import Feed
 
 
@@ -17,3 +23,26 @@ class Main(APIView):
             print(feed.content)
 
         return render(request, "instagramClone/main.html", context=dict(feed_list=feed_list))
+
+
+class UploadFeed(APIView):
+    def post(self, request):
+        # 파일 불러오기
+        file = request.FILES['file']
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        file = request.data.get('file')
+        image = uuid_name
+        content = request.data.get('content')
+        user_id = request.data.get('user_id')
+        profile_image = request.data.get('profile_image')
+
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+
+        return Response(status=200)
--- instagramClone/urls.py
+++ instagramClone/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from .views import Sub
-from content.views import Main
+from django.urls import path, include
+
+from . import settings
+from content.views import Main, UploadFeed
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', Main.as_view())
-
+    path('main/', Main.as_view()),
+    # path('', UploadFeed.as_view())
+    path('', include('content.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
